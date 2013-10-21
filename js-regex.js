b88ed7cba9bb2006f981ceb177d7e82ecc226614
mingyang.wu <mingyang.wu@edoctor.cn>
2013-10-21 15:22:52 +0800

    fix isArray

--- js-regex.js
+++ js-regex.js
@@ -22,7 +22,7 @@
   }
 
   function isArray (obj) {
-    return Object.prototype.toString.call(obj) === '[object array]';
+    return Object.prototype.toString.call(obj) === '[object Array]';
   }
 
 
